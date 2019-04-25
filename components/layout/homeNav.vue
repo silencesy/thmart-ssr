122da736b0b265cd3adb72442c03669b3bc7d995
Your Name <you@example.com>
2019-04-25 10:17:12 +0800

    4.25导航栏不贴边

--- components/layout/homeNav.vue
+++ components/layout/homeNav.vue
@@ -129,127 +129,128 @@
 </script>
 
 <style lang='sass' type="text/css" scoped>
-    @import '~/assets/sass/common.sass'
-    .homeHead
-        margin: 15px 0
-        overflow: hidden
+@import '~/assets/sass/common.sass'
+.homeHead
+    margin: 15px 0
+    overflow: hidden
+    @include hh(80px, 80px)
+    >div
+        float: left
+    .left, .right
+        img
+            vertical-align: middle
+    .left
+        img
+            cursor: pointer
+            @include wh(80px, 60px)
+    .right
+        float: right
+        img
+            @include wh(80px, 80px)
+    .center
+        position: relative
+        margin-left: 23%
         @include hh(80px, 80px)
-        >div
+        i
+            position: absolute
+            color: #999
+            left: 13px
+            top: 3px
+            z-index: -1
+        input
+            @include whh(380px, 40px, 40px)
+            color: $describe_color
+            @include ttbb(4px, 0, 4px, 0) 
+            padding: 0 10px 
+            background: transparent  
+            box-sizing: border-box
+        button
+            background-color: $theme_color
+            @include sc(16px, #fff)
+            @include whh(120px, 40px, 40px)
+            @include ttbb(0, 4px, 0, 4px)
+            cursor: pointer
+            vertical-align: top
+            margin-top: 21px 
+.homeNav
+    @include hh(40px, 40px)
+    ul  
+        overflow: hidden
+        li 
             float: left
-        .left, .right
-            img
-                vertical-align: middle
-        .left
-            img
-                cursor: pointer
-                @include wh(80px, 60px)
-        .right
-            float: right
-            img
-                @include wh(80px, 80px)
-        .center
-            position: relative
-            margin-left: 23%
-            @include hh(80px, 80px)
-            i
-                position: absolute
-                color: #999
-                left: 13px
-                top: 3px
-                z-index: -1
-            input
-                @include whh(380px, 40px, 40px)
-                color: $describe_color
-                @include ttbb(4px, 0, 4px, 0) 
-                padding: 0 10px 
-                background: transparent  
-                box-sizing: border-box
-            button
-                background-color: $theme_color
-                @include sc(16px, #fff)
-                @include whh(120px, 40px, 40px)
-                @include ttbb(0, 4px, 0, 4px)
-                cursor: pointer
-                vertical-align: top
-                margin-top: 21px 
-    .homeNav
-        @include hh(40px, 40px)
-        ul  
-            overflow: hidden
-            li 
-                float: left
-                a
-                    padding: 0 62px 
-                    display: inline-block
-                    @include hh(40px, 40px)
-            li:first-child
-                @include wc(239px, center)
-                background-color: $theme_color               
-                a
-                    color: #fff
-                    padding: 0
-                    width: 100% 
-                i   
-                    padding-right: 15px
-            li:nth-child(2)
-                a
-                    padding-left: 123px
-                    padding-right: 48px
-            li:nth-child(3)
-                a
-                    padding-left: 78px
-                    padding-right: 58px
-            li:last-child
-                a 
-                    padding-right: 0
-    .nav
-        .line
-            border-bottom: 1px solid $theme_color
-    .allCateBox
+            a
+                padding: 0 62px 
+                display: inline-block
+                @include hh(40px, 40px)
+        li:first-child
+            @include wc(239px, center)
+            background-color: $theme_color               
+            a
+                color: #fff
+                padding: 0
+                width: 100% 
+            i   
+                padding-right: 15px
+        li:nth-child(2)
+            a
+                padding-left: 123px
+                padding-right: 48px
+        li:nth-child(3)
+            a
+                padding-left: 78px
+                padding-right: 58px
+        li:last-child
+            a 
+                padding-right: 0
+.nav
+    .line
+        border-bottom: 1px solid $theme_color
+.allCateBox
+    width: 100%
+    height: 100%
+    position: relative
+    .allCate
         width: 100%
         height: 100%
-        position: relative
-        .allCate
-            width: 100%
-            height: 100%
-            display: block
-    .el-dropdown-menu 
-        overflow: hidden
-        background-color: rgba(0, 0, 0, .8)
-        border: none
-        right: 0
-        margin-top: -1px
-        height: 340px
-        .dropdownBox
-            width: 1200px
-            margin: 0 auto
-            padding: 0 38px
-            .listPer
-                float: left
-                width: 50%
-                padding-top: 5px
-                li 
-                    padding-left: 15px
-                    color: #fff
-                    padding-top: 0
-                    height: 30px
-                    line-height: 30px
-                    font-size: 16px
-                    display: inline-block
-                .smallTitle
-                    display: inline
-                    li
-                        color: #ffffffd1
-                        font-size: 14px
-                        height: 26px
-                        line-height: 26px
-                li:hover
-                    background-color: rgba(0,0,0,0)
-                    color: $theme_color
-                li:active 
-                    background-color: rgba(0,0,0,0)
-                li:first-child
-                    padding-top: 2px
-                >div 
-                    width: 100%
+        display: block
+.el-dropdown-menu 
+    overflow: hidden
+    background-color: rgba(0, 0, 0, .8)
+    border: none
+    right: 0
+    margin-top: -1px
+    height: 340px
+    margin-left: -5px
+    .dropdownBox
+        width: 1200px
+        margin: 0 auto
+        padding: 0 38px
+        .listPer
+            float: left
+            width: 50%
+            padding-top: 5px
+            li 
+                padding-left: 15px
+                color: #fff
+                padding-top: 0
+                height: 30px
+                line-height: 30px
+                font-size: 16px
+                display: inline-block
+            .smallTitle
+                display: inline
+                li
+                    color: #ffffffd1
+                    font-size: 14px
+                    height: 26px
+                    line-height: 26px
+            li:hover
+                background-color: rgba(0,0,0,0)
+                color: $theme_color
+            li:active 
+                background-color: rgba(0,0,0,0)
+            li:first-child
+                padding-top: 2px
+            >div 
+                width: 100%
 </style>
